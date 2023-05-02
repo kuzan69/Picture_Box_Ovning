88834f11b49e5038e54ea3f823ceccf1207f3b88
kuzan69 <113890965+kuzan69@users.noreply.github.com>
2023-05-02 11:51:52 +0200

    Add files via upload
    Hej

---
+++ Picture Box Övning/Picture Box Övning/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Picture_Box_Övning
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox4.Image = Image.FromFile(@"C:\Users\yosef.shiervani\Downloads\Here comes the star-yoi.jpg");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox5.Image = Image.FromFile(@"C:\Users\yosef.shiervani\Downloads\Here comes the star-yoi.jpg");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string filePath = openFileDialog1.FileName;
+            pictureBox6.Image = Image.FromFile(filePath);
+        }
+    }
+}
